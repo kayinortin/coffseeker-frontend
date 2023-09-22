841054cb57ba191af56b14f9765bf094398b7824
Jenny <dias3839@gmail.com>
2023-09-22 13:29:26 +0800

    購物車步驟

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,25 +1,40 @@
 import React from 'react'
+import { BiSolidRightArrow } from 'react-icons/Bi'
+import Step from '@/components/cart/step'
 
 export default function Cart() {
   return (
     <>
-      <div className={'container'}>
-        <h1>購物車</h1>
-        <div className={'step'}>
-          <div className={'stepTop'}>
-            <div className={'stepCircle'}></div>
-            <div className={'setNum'}>1</div>
-          </div>
-          <div className={'stepFont'}>確認購物清單</div>
+      <section className={'background'}>
+        <div className={'container'}>
+          {/* <div className={'step'}>
+            <div className={'step1'}>
+              <div className={'stepTop'}>
+                <div className={'stepCircle circleBlue'}></div>
+                <div className={'setNum setnumWhite'}>1</div>
+              </div>
+              <div className={'stepFont stepfontBlue'}>確認購物清單</div>
+            </div>
+            <BiSolidRightArrow className={'arrow arrowBlue'} />
+            <div className={'step2'}>
+              <div className={'stepTop'}>
+                <div className={'stepCircle circleGray'}></div>
+                <div className={'setNum setnumGray'}>2</div>
+              </div>
+              <div className={'stepFont stepfontGray'}>填寫資料</div>
+            </div>
+            <BiSolidRightArrow className={'arrow arrowGray'} />
+            <div className={'step3'}>
+              <div className={'stepTop'}>
+                <div className={'stepCircle circleGray'}></div>
+                <div className={'setNum setnumGray'}>3</div>
+              </div>
+              <div className={'stepFont stepfontGray'}>訂單確認</div>
+            </div>
+          </div> */}
+          <Step />
         </div>
-        <div className={'step'}>
-          <div className={'stepTop'}>
-            <div className={'stepCircle circle-gray'}></div>
-            <div className={'setNum'}>2</div>
-          </div>
-          <div className={'stepFont'}>確認購物清單</div>
-        </div>
-      </div>
+      </section>
     </>
   )
 }
