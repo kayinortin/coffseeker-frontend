751ce0c70266fb84621110f064f107e81c911b70
HowardddYuan <127181493+HowardddYuan@users.noreply.github.com>
2023-09-24 23:25:40 +0800

    字體粗細測試

--- components/course/BreadCrumbs.js
+++ components/course/BreadCrumbs.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function BreadCrumbs() {
   return (
     <>
-      <nav aria-label="breadcrumb">
+      <span aria-label="breadcrumb ms-5">
         <ol className="breadcrumb">
           <li className="breadcrumb-item">
             <a href="#">首頁</a>
@@ -12,7 +12,7 @@ export default function BreadCrumbs() {
             <a href="#">課程列表</a>
           </li>
         </ol>
-      </nav>
+      </span>
     </>
   )
 }
--- components/course/CourseList.js
+++ components/course/CourseList.js
@@ -2,15 +2,15 @@ import React from 'react'
 import data from '@/data/course/course.json'
 import Card from './Card'
 import Breadcrumb from './BreadCrumbs'
+import Pagination from './Pagination'
 
 export default function CourseList() {
   // console.log(data)
   return (
     <div className="">
-      <Breadcrumb className="ms-5" />
-      <h3 className="text-center">課程列表</h3>
-
-      <ul className="d-flex row row-cols-3 flex-wrap list-unstyled">
+      <h3 className="text-center mt-5">課程列表</h3>
+      <Breadcrumb className="" />
+      <ul className=" row row-cols-sm-3 flex-wrap list-unstyled">
         {data.map((v, i) => {
           let {
             name,
@@ -21,27 +21,24 @@ export default function CourseList() {
             description,
             image,
           } = v
-          {
-            /* console.log(v) */
-          }
+
           return (
-            <>
-              <li key={i} className="col course-li">
-                <Card
-                  key={i}
-                  name={name}
-                  price={price}
-                  start_date={start_date}
-                  end_date={end_date}
-                  capacity={capacity}
-                  description={description}
-                  image={image}
-                />
-              </li>
-            </>
+            <li key={i} className=" d-flex justify-content-center course-li">
+              <Card
+                // key={i}
+                name={name}
+                price={price}
+                start_date={start_date}
+                end_date={end_date}
+                capacity={capacity}
+                description={description}
+                image={image}
+              />
+            </li>
           )
         })}
       </ul>
+      <Pagination />
     </div>
   )
 }
