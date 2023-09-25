5f5d878d517997ff0fed7c8269c5991bf7d2e8f5
Jenny <dias3839@gmail.com>
2023-09-25 14:30:40 +0800

    購物車樣式調整

--- components/cart/CartList.js
+++ components/cart/CartList.js
@@ -1,18 +1,61 @@
 import React from 'react'
+import { RiDeleteBin6Line } from 'react-icons/Ri'
 
 function CartList() {
   return (
     <>
-      {' '}
-      <div>CartList</div>
-      <ul>
-        <li>1</li>
-        <li>1</li>
-        <li>1</li>
-        <li>1</li>
-        <li>1</li>
-        <li>1</li>
-      </ul>
+      <table className={'carts'}>
+        <thead className={'cartLabels'}>
+          <tr>
+            <th
+              className={'align-middle p-6 text-start'}
+              colSpan="2"
+              scope="col"
+            >
+              項目
+            </th>
+            <th className={'align-middle'} scope="col">
+              數量
+            </th>
+            <th className={'align-middle'} scope="col">
+              單價
+            </th>
+            <th className={'align-middle'} scope="col">
+              折扣價
+            </th>
+            <th className={'align-middle'} scope="col">
+              小計
+            </th>
+            <th className={'align-middle'} scope="col">
+              刪除
+            </th>
+          </tr>
+        </thead>
+        <tbody className={'cartProducts'}>
+          <tr>
+            <td className={'align-middle'}>
+              <img
+                className={'img-fluid'}
+                src="http://placehold.it/160x160"
+                alt="product"
+              />
+            </td>
+            <td className={'align-middle text-start'}>
+              <div className={'fs-4 mb-2'}>商品名稱</div>
+              <div className={'fs-6'}>
+                商品詳情商品詳情商品詳情商品詳情商品詳情商品詳情商品詳情商品詳情商品詳情商品詳情商品詳情商品詳情商品詳情商品詳情
+              </div>
+            </td>
+            <td className={'align-middle'}>+1</td>
+            <td className={'align-middle'}>$999</td>
+            <td className={'align-middle'}>$900</td>
+            <td className={'align-middle'}>$900</td>
+            <td className={'align-middle'}>
+              <RiDeleteBin6Line />
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </>
   )
 }
