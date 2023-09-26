73cbeffa0b7edeac121ee3ade2b3516c9582a14c
RockyJade <rockyjade0514@gmail.com>
2023-09-27 01:57:14 +0800

    占卜頁面新增

---
+++ pages/infomation/divination/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import DivinationIndex from '@/components/divination'
+export default function Divination() {
+  return (
+    <>
+      <DivinationIndex />
+    </>
+  )
+}
