c05a7fe42bf656fce973f8cc79e1ea0e83567c25
Jenny <dias3839@gmail.com>
2023-09-27 10:35:58 +0800

    購物車折扣調整、新增無商品狀態

--- components/cart/CartList.js
+++ components/cart/CartList.js
@@ -1,44 +1,70 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { RiDeleteBin6Line } from 'react-icons/ri'
 import { GrAdd } from 'react-icons/gr'
 import { AiOutlineMinus } from 'react-icons/ai'
 import productsData from '@/data/cart/cart'
 
+//購物清單總金額
 function calculateTotal(cart) {
   const total = cart.reduce((accumulator, product) => {
-    return accumulator + product.quantity * product.discountPrice // 计算总价时考虑数量和折扣价格
+    // 計算總金額，考慮產品數量和折扣價格
+    return accumulator + product.quantity * product.discountPrice
   }, 0)
-  return total.toFixed() // 保留两位小数
+  return Math.round(total.toFixed(2))
 }
-
+//計算折扣
 function applyDiscount(price, discountRate) {
-  return price * discountRate
+  const parsedPrice = parseFloat(price)
+  //檢查折扣是否為NaN
+  if (isNaN(parsedPrice)) {
+    return (0).toFixed(2)
+  }
+  const discountedPrice = parsedPrice * discountRate
+  return Math.round(discountedPrice.toFixed(2))
 }
-
+//購物清單的
 function CartList() {
-  const [cart, setCart] = useState(productsData)
-  const [discountRate, setDiscountRate] = useState(0.9) // 默认折扣为九折
+  const [cart, setCart] = useState([]) //儲存購物車商品清單
+  const [discountRate, setDiscountRate] = useState(0.85) //設定初始折扣率
+
+  //初始化購物車，將商品與折扣價格一起放到購物車內
+  const initCarDiscount = () => {
+    const cartWithDiscount = productsData.map((product) => ({
+      ...product,
+      discountPrice: applyDiscount(product.price, discountRate), //更新每個產品的價格
+    }))
+    setCart(cartWithDiscount)
+  }
+
+  //discountRate狀態發生改變的時候，會觸發useEffect執行函式，因此得到更新後的折扣價格
+  useEffect(() => {
+    initCarDiscount()
+  }, [discountRate])
 
   const handleQuantityChange = (productId, changeAmount) => {
-    const updatedCart = cart.map((product) => {
-      if (product.id === productId) {
-        // 更新数量，确保不低于1
-        const newQuantity = Math.max(1, product.quantity + changeAmount)
-
-        // 计算新的折扣价格，基于单价和折扣率
-        const newDiscountPrice = applyDiscount(product.price, discountRate)
-
-        return {
-          ...product,
-          quantity: newQuantity,
-          discountPrice: newDiscountPrice,
+    if (changeAmount === 0) {
+      // 删除商品
+      const updatedCart = cart.filter((product) => product.id !== productId)
+      setCart(updatedCart)
+    } else {
+      // 更新商品数量和折扣价格
+      const updatedCart = cart.map((product) => {
+        if (product.id === productId) {
+          const newQuantity = Math.max(1, product.quantity + changeAmount)
+          const newDiscountPrice = applyDiscount(product.price, discountRate)
+          return {
+            ...product,
+            quantity: newQuantity,
+            discountPrice: newDiscountPrice,
+          }
         }
-      }
-      return product
-    })
-
-    setCart(updatedCart)
+        return product
+      })
+      setCart(updatedCart)
+    }
   }
+
+  const isCartEmpty = cart.length === 0
 
   const productItems = cart.map((product) => (
     <tr key={product.id} className="">
@@ -104,45 +130,53 @@ function CartList() {
   return (
     <>
       <div className="cartlist">
-        <table className={'carts'}>
-          <thead className={'cartLabels'}>
-            <tr>
-              <th
-                className={'align-middle text-start ps-4'}
-                colSpan="2"
-                scope="col"
-              >
-                項目
-              </th>
-              <th className={'align-middle'} scope="col">
-                數量
-              </th>
-              <th className={'align-middle'} scope="col">
-                單價
-              </th>
-              <th className={'align-middle'} scope="col">
-                折扣
-              </th>
-              <th className={'align-middle'} scope="col">
-                小計
-              </th>
-              <th className={'align-middle'} scope="col">
-                刪除
-              </th>
-            </tr>
-          </thead>
-          {/* 產品列表 */}
-          <tbody className={'cartProducts'}>{productItems}</tbody>
-          {/* 總額 */}
-          <tfoot>
-            <tr className="carttotal">
-              <td colSpan="5" className="text-end total">
-                總額：
-              </td>
-              <td className={'align-middle'}>${calculateTotal(cart)}</td>
-            </tr>
-          </tfoot>
-        </table>
+        {isCartEmpty ? (
+          <>
+            <div className="text-center">
+              <h3>您的購物車目前無商品</h3>
+            </div>
+          </>
+        ) : (
+          <table className={'carts'}>
+            <thead className={'cartLabels'}>
+              <tr>
+                <th
+                  className={'align-middle text-start ps-4'}
+                  colSpan="2"
+                  scope="col"
+                >
+                  項目
+                </th>
+                <th className={'align-middle'} scope="col">
+                  數量
+                </th>
+                <th className={'align-middle'} scope="col">
+                  單價
+                </th>
+                <th className={'align-middle'} scope="col">
+                  折扣
+                </th>
+                <th className={'align-middle'} scope="col">
+                  小計
+                </th>
+                <th className={'align-middle'} scope="col">
+                  刪除
+                </th>
+              </tr>
+            </thead>
+            {/* 產品列表 */}
+            <tbody className={'cartProducts'}>{productItems}</tbody>
+            {/* 總額 */}
+            <tfoot>
+              <tr className="carttotal">
+                <td colSpan="5" className="text-end total">
+                  總額：
+                </td>
+                <td className={'align-middle'}>${calculateTotal(cart)}</td>
+              </tr>
+            </tfoot>
+          </table>
+        )}
       </div>
     </>
   )
