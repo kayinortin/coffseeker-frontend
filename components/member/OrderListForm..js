ff65eb6d9822117ebe564f252ac1bbde58ee06ce
Minamura <zwilmin860924@gmail.com>
2023-09-30 01:39:28 +0800

    會員登入token存入cookie完成 訂單頁面切版完成

--- components/member/OrderListForm..js
+++ components/member/OrderListForm..js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { FaAngleDown } from 'react-icons/fa6'
 import Link from 'next/link'
-import OrderItems from './OrderItems'
+import OrderdetailOpened from './OrderdetailOpened'
 
 export default function OrderListForm() {
   const fakeUser = {
@@ -27,16 +27,16 @@ export default function OrderListForm() {
       <div className={'table-box border border-dark'}>
         <div className={'form-title border-bottom border-dark p-3 d-flex'}>
           <span className="col-4">訂單管理</span>
-          <div className="col-8 d-flex justify-content-between">
-            <span className="">訂單日期</span>
-            <span className="">訂單總額</span>
-            <span className="">訂單狀態</span>
-            <span className="">付款狀態</span>
-            <span className="">訂單明細</span>
+          <div className="col-8 d-flex justify-content-between order-nav">
+            <div className="text-center">訂單日期</div>
+            <div className="text-center">訂單總額</div>
+            <div className="text-center">訂單狀態</div>
+            <div className="text-center">付款狀態</div>
+            <div className="text-center">訂單明細</div>
           </div>
         </div>
         {/*  */}
-        <OrderItems />
+        <OrderdetailOpened />
         {/*  */}
       </div>
     </>
