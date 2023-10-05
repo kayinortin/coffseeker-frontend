eb39923bf7dc41d85b0ccef61e49437e13fded9d
Edison <kayinortin@hotmail.com>
2023-10-05 22:25:50 +0800

    新增keydown

--- components/Counter.js
+++ components/Counter.js
@@ -2,26 +2,58 @@ import { useState } from 'react'
 
 function Counter(props) {
   const { number, setNumber, show } = props
+  const [isFocused, setIsFocused] = useState(false)
+
+  const handleMinusClick = () => {
+    number <= 1 ? setNumber(1) : setNumber(number - 1)
+  }
+
+  const handlePlusClick = () => {
+    setNumber(number + 1)
+  }
+
+  const handleFocus = () => {
+    setIsFocused(true)
+  }
+
+  const handleBlur = () => {
+    setIsFocused(false)
+  }
+
   return (
     <>
-      <div className={show ? 'ed-counter w-50 mobile-counter' : 'ed-counter'}>
+      <div
+        className={show ? 'ed-counter w-50 mobile-counter' : 'ed-counter'}
+        role="button"
+        tabIndex="0"
+        onClick={handleFocus}
+        onKeyDown={(event) => {
+          if (event.key === 'Enter' || event.key === ' ') {
+            handleFocus()
+          }
+        }}
+        onBlur={handleBlur}
+      >
         <button
           type="button"
           className="ed-counter__btn"
-          onClick={() => {
-            number <= 1 ? setNumber(1) : setNumber(number - 1)
-          }}
+          onClick={handleMinusClick}
         >
           <i className="fas fa-minus e-icon ed-counter__icon"></i>
         </button>
-        {number}
-        <button type="button" className="ed-counter__btn">
-          <i
-            className="fas fa-plus e-icon ed-counter__icon"
-            onClick={() => {
-              setNumber(number + 1)
-            }}
-          ></i>
+        <span
+          className={
+            isFocused ? 'ed-counter__number focused' : 'ed-counter__number'
+          }
+        >
+          {number}
+        </span>
+        <button
+          type="button"
+          className="ed-counter__btn"
+          onClick={handlePlusClick}
+        >
+          <i className="fas fa-plus e-icon ed-counter__icon"></i>
         </button>
       </div>
     </>
