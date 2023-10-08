1ebacb6dbfb2ad4a09e1b4f4edba974ab178fa9c
Edison <kayinortin@hotmail.com>
2023-10-09 04:39:09 +0800

    facebook message setting

--- pages/_document.js
+++ pages/_document.js
@@ -35,9 +35,36 @@ export default function Document() {
       <link rel="manifest" href="http://localhost:3000/manifest.json" />
       <Head />
       <body>
+        {/* Messenger Chat Plugin Code */}
+        <div id="fb-root"></div>
+        <div id="fb-customer-chat" className="fb-customerchat"></div>
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            var chatbox = document.getElementById('fb-customer-chat');
+            chatbox.setAttribute("page_id", "162999953559227");
+            chatbox.setAttribute("attribution", "biz_inbox");
+            
+            window.fbAsyncInit = function() {
+              FB.init({
+                xfbml            : true,
+                version          : 'v18.0'
+              });
+            };
+
+            (function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s); js.id = id;
+              js.src = 'https://connect.facebook.net/zh_TW/sdk/xfbml.customerchat.js';
+              fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+          `,
+          }}
+        />
         <Main />
         <NextScript />
-        
       </body>
     </Html>
   )
