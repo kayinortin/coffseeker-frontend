8924243a6ea2041ed87955032e00a1bcd24fa5cb
Eileen Chang <133978195+Yi-Ching-Chang@users.noreply.github.com>
2023-10-11 17:05:12 +0800

    手沖介紹完成

--- pages/infomation/handbrewed/index.js
+++ pages/infomation/handbrewed/index.js
@@ -2,7 +2,17 @@ import React from 'react'
 import HandBrewCard from '@/components/handbrewed/handbrewed-card'
 
 const HandBrew = () => {
-  return <HandBrewCard />
+  return (
+    <>
+      {/* 標題區 */}
+      <div className="d-flex justify-content-center my-4 align-items-center mobile-news-title">
+        <div className="ei-line me-3"></div>
+        <h3 className="text-center news-title fs-2">手沖介紹</h3>
+        <div className="ei-line ms-3"></div>
+      </div>
+      <HandBrewCard />
+    </>
+  )
 }
 
 export default HandBrew
