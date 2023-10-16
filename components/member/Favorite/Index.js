08f4589a5c739d86434147ce187ec8f03d7d12ad
RockyJade <rockyjade0514@gmail.com>
2023-10-16 14:23:38 +0800

    收藏：未登入會員連結跳轉

--- components/member/Favorite/Index.js
+++ components/member/Favorite/Index.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
 import Swal from 'sweetalert2'
 import axios from 'axios'
 import { useUser } from '@/context/UserInfo'
@@ -7,7 +8,52 @@ import RemoveFavProduct from '@/components/fav/RemoveFavProduct'
 import RemoveFavCourse from '@/components/fav/RemoveFavCourse'
 import _ from 'lodash'
 import Head from 'next/head'
+import { FetchUserData } from '../FetchDatas/FetchUserData'
+import Cookies from 'js-cookie'
+
 export default function Favorite() {
+  //檢查登入狀態，未登入跳轉登入頁
+  const { userData, setUserData } = useUser()
+  const router = useRouter()
+  const [userId, setId] = useState('')
+  const [userEmail, setMail] = useState('')
+  const [userName, setName] = useState('')
+  const [userPhone, setPhone] = useState('')
+  const [userAddress, setAddress] = useState('')
+  const [userGender, setGender] = useState('')
+  const [birthdayYear, setBirthdayYear] = useState('')
+  const [birthdayMonth, setBirthdayMonth] = useState('')
+  const [birthdayData, setBirthdayDate] = useState('')
+
+  const checkToken = Cookies.get('accessToken')
+  useEffect(() => {
+    async function fetchData() {
+      if (checkToken) {
+        const fetchUser = await FetchUserData()
+        console.log(fetchUser)
+        if (fetchUser) {
+          setUserData(fetchUser)
+          setId(fetchUser.id)
+          setMail(fetchUser.email)
+          setName(fetchUser.username)
+          setPhone(fetchUser.phone)
+          setGender(fetchUser.gender)
+          setAddress(fetchUser.address)
+          const UserBirthday = fetchUser.birthday.split('-')
+          setBirthdayYear(UserBirthday[0])
+          setBirthdayMonth(UserBirthday[1])
+          setBirthdayDate(UserBirthday[2])
+        }
+      } else {
+        console.log('Cookie不存在')
+        router.push('/member/login')
+      }
+    }
+
+    fetchData()
+  }, [])
+
+  //取得收藏商品/課程資料
   const [favProductData, setFavProductData] = useState(null)
   const [favCourseData, setFavCourseData] = useState(null)
 
@@ -60,12 +106,15 @@ export default function Favorite() {
     fetchFavCourse()
   }, [])
 
+  //移除收藏商品
   function handleRemoveFavProduct(id) {
     const updatedFavProductData = _.cloneDeep(favProductData)
     _.remove(updatedFavProductData, (product) => product.id === id)
     RemoveFavProduct(id)
     setFavProductData(updatedFavProductData)
   }
+
+  //移除收藏課程
   function handleRemoveFavCourse(id) {
     const updatedFavCourseData = _.cloneDeep(favCourseData)
     _.remove(updatedFavCourseData, (course) => course.id === id)
